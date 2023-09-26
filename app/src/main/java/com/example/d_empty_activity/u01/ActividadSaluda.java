86d0c91ad0b7f0ec1c6f4f3a9a9428066078ea5e
fausto.manotoa <famt.docs@gmail.com>
2023-09-26 17:50:08 +0200

    Mi Primera Actividad

---
+++ app/src/main/java/com/example/d_empty_activity/u01/ActividadSaluda.java
@@ -0,0 +1,16 @@
+package com.example.d_empty_activity.u01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.d_empty_activity.R;
+
+public class ActividadSaluda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad_saluda);
+    }
+}
